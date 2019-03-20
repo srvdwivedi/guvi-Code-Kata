d43770cab30fbd8d22e63af3c42328beec11a4a9
saurav dwivedi <souravdwivedi999@gmail.com>
2019-03-20 11:03:28 +0530

    Delete Check_number.py

--- Check_number.py
+++
@@ -1,9 +0,0 @@
-def number(num):
-    if num > 0:
-        print("Positive")
-    elif num == 0:
-        print("Zero")
-    else:
-        print("Negative")
-num = int(input())      
-print(number(num))
