2e5a65df3c71d4fe4e96838be76ce817f2acd23f
saurav dwivedi <souravdwivedi999@gmail.com>
2019-03-20 11:34:07 +0530

    Update Odd_Even.py

--- Odd_Even.py
+++ Odd_Even.py
@@ -1,7 +1,13 @@
-def number(num):
-    if num%2 == 0:
-        print("Even")
-    else:
-        print("Odd")
-num = int(input())
-print(number(num))
+def number():
+    try:
+        num=int(input())
+           
+        if(num % 2 == 0):
+            print("Even")
+        elif num < 0:
+            print("invalid")
+        else:
+            print("Odd")
+    except ValueError as e:
+        print("invalid")
+number()
