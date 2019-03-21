a8cba3ff3a6f6c651586a185dfb865e2b6e7ac18
saurav dwivedi <souravdwivedi999@gmail.com>
2019-03-21 13:52:48 +0530

    Create Leap.py

---
+++ Leap.py
@@ -0,0 +1,10 @@
+def leap():
+    try:
+        year=int(input())
+        if(year%4==0 and year%100!=0 or year%400==0):
+            print("yes")
+        else:
+            print("no")
+    except ValueError:
+        print("invalid")
+leap()
