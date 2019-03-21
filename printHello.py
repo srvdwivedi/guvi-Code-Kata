9903cfb77a8839cf7f0dc44fe72c9b140e95f279
saurav dwivedi <souravdwivedi999@gmail.com>
2019-03-21 14:16:28 +0530

    Update printHello.py

--- printHello.py
+++ printHello.py
@@ -2,7 +2,7 @@ def loop():
     try:
         n = int(input())
         for i in range(n):
-            print("Hello",end=" ")
+            print("Hello\n")
     except ValueError as e:
         print("invalid")
 loop()
