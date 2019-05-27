f52ec0ee157aaf147251971270ecadc9291843a9
SRV <souravdwivedi999@gmail.com>
2019-05-27 19:10:14 +0530

    Create colinear.py

---
+++ colinear.py
@@ -0,0 +1,10 @@
+def collinear(x1, y1, x2, y2, x3, y3): 
+    if ((y3 - y2)*(x2 - x1) == (y2 - y1)*(x3 - x2)): 
+        return ("yes")
+    else:
+        return ("no")
+x1,y1 = map(int,input().split())
+x2,y2 = map(int,input().split())
+x3,y3 = map(int,input().split())
+
+collinear(x1, y1, x2, y2, x3, y3)
