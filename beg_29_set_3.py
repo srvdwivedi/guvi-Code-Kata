8448a250956197dbe590e93c6a72aae35f79b5cd
SRV <souravdwivedi999@gmail.com>
2019-05-27 15:42:05 +0530

    Create beg_29_set_3.py

---
+++ beg_29_set_3.py
@@ -0,0 +1,8 @@
+num = int(input())
+if num < 60:
+    print('0'+" "+str(num))
+else:
+    if num>=60:
+        hours = num // 60
+        minutes = num % 60
+        print("{} {} ".format(hours, minutes))
