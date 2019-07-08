5db5738f405f4fcfa3e528b7756be55ae8f9db8f
SRV <souravdwivedi999@gmail.com>
2019-07-08 22:39:55 +0530

    Create pro23.py

---
+++ pro23.py
@@ -0,0 +1,13 @@
+import statistics as st
+ana=int(input())
+l=list(map(int,input().split()))
+cac=0
+for i in range(1,ana):
+    l1=l[0:i]
+    l2=l[i:ana]
+    if(st.mean(l1)==st.mean(l2)):
+        cac=cac+1
+if(cac>=1):
+    print("yes")
+else:
+    print("no"
