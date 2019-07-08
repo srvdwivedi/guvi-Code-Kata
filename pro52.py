93161a6d0bde5ae7d68eaa0cc4c1506e412c538b
SRV <souravdwivedi999@gmail.com>
2019-07-08 22:32:46 +0530

    Create pro52.py

---
+++ pro52.py
@@ -0,0 +1,8 @@
+a1,b1=map(int,input().split())
+c1,d1=map(int,input().split())
+e1,f1=map(int,input().split())
+g1,h1=map(int,input().split())
+if(a1==c1 and e1==g1 and b1==h1 and d1==f1):
+    print("yes")
+else:
+    print("no")
