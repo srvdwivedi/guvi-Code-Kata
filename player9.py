701150094e0e1d134a8a905c4be4298233867fdb
SRV <souravdwivedi999@gmail.com>
2019-07-10 11:16:46 +0530

    Create player9.py

---
+++ player9.py
@@ -0,0 +1,11 @@
+start,stop = map(int,input().split())
+c = 0
+for i in range(start+1,stop):
+    f = 0
+    for j in range(2,i):
+        if i % j == 0:
+            f = 1
+            break
+    if f == 0:
+        c +=1
+print(c)
